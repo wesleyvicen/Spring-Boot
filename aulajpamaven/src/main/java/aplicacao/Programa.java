0dbb7d58b91e97aaf05cd35a78b8ca03d44835eb
Wesley Vicente <wesley1535@hotmail.com>
2020-10-23 22:46:45 -0300

    Usado o find para buscar e monitorando para remover

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -11,18 +11,27 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		/* Instanciando para inserir no banco */
-		Pessoa p1 = new Pessoa(null, "Wesley Vicente da Silva", "wesley1535@hotmail.com");
-		Pessoa p2 = new Pessoa(null, "Aline Fernanda Gomes Oliveira", "aalineet@hotmail.com");
-		Pessoa p3 = new Pessoa(null, "Welson Vicente da Silva", "welson1535@hotmail.com");
+//		Pessoa p1 = new Pessoa(null, "Wesley Vicente da Silva", "wesley1535@hotmail.com");
+//		Pessoa p2 = new Pessoa(null, "Aline Fernanda Gomes Oliveira", "aalineet@hotmail.com");
+//		Pessoa p3 = new Pessoa(null, "Welson Vicente da Silva", "welson1535@hotmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-
+		
 		/* Inserindo no banco */
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+
+		
+		/* Buscando uma pessoa do ID 3 usando o Find */
+		Pessoa p = em.find(Pessoa.class, 3);
+		
+		/* Removendo do banco Monitorando, usando o find */
 		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		em.remove(p);
 		em.getTransaction().commit();
 		
 		System.out.println("Pronto!");
