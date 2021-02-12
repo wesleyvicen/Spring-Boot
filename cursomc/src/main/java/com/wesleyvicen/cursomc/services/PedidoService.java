6120c5e7a057d997599e3c1f60f2ed71d16f9992
Wesley Vicente <wesley1535@hotmail.com>
2021-02-12 17:02:14 -0300

    endpoint /pedidos/{id} disponibilizado

---
+++ cursomc/src/main/java/com/wesleyvicen/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.wesleyvicen.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesleyvicen.cursomc.domain.Pedido;
+import com.wesleyvicen.cursomc.repositories.PedidoRepository;
+import com.wesleyvicen.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto Não encontrado! ID: "+ id + ", Tipo: "+ Pedido.class.getName())); 
+	}
+	
+}
